cbd5f61cacc6619b49e98e17f46ba912e03e4df6
Dan Searle <dan.searle@ft.com>
2013-04-09 17:31:13 +0100

    IP957. Added missing grid.scss from ft-cms, moved variables files into separate folder.

--- src/main/config/wroCombiningCSS.groovy
+++ src/main/config/wroCombiningCSS.groovy
@@ -2,6 +2,7 @@ groups {
 
     "ft-velcro" {
         css(minimize:false, "/distribution-helper/**.scss")
+        css(minimize:false, "/sources/variables/**.scss")
         css(minimize:false, "/sources/**.scss")
     }
 

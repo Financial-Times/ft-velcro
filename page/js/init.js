aa48f29d7169f27daef3c507ab0b9ca5ca3622c4
Dan Searle <dan.searle@ft.com>
2013-04-09 16:44:55 +0100

    IP957. Deleted duplicated Github page code, updated colors variables.

--- page/js/init.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-
-    // Stop empty links (in code examples) scrolling the page to the top.
-    $(document).on("click", "a[href=#]", function (ev) {
-        ev.preventDefault();
-    });
-
-});

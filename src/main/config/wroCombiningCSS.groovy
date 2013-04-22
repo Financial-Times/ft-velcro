46b5b7930f475964306d61f724056727e44f7f7a
Roland Vachter <roland.vachter@ft.com>
2013-04-22 16:55:32 +0300

    ft-cms reorganized for a better inclusion in ft-cms. It is meant to resolve the namespacing of images and for serving the images by the ft-cms.

--- src/main/config/wroCombiningCSS.groovy
+++ src/main/config/wroCombiningCSS.groovy
@@ -1,8 +1,9 @@
 groups {
 
     "ft-velcro" {
-        css(minimize:false, "/distribution-helper/**.scss")
+        css(minimize:false, "/distribution-helper/base-variables.scss")
         css(minimize:false, "/sources/variables/**.scss")
+        css(minimize:false, "/distribution-helper/overrides.scss")
         css(minimize:false, "/sources/**.scss")
     }
 

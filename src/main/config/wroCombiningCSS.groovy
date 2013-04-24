352a5fe69f54928b8fe16fdbff74e0df5db77025
Roland Vachter <roland.vachter@ft.com>
2013-04-24 14:20:03 +0300

    Distribution paths fixed.

--- src/main/config/wroCombiningCSS.groovy
+++ src/main/config/wroCombiningCSS.groovy
@@ -3,6 +3,7 @@ groups {
     "ft-velcro" {
         css(minimize:false, "/distribution-helper/base-variables.scss")
         css(minimize:false, "/sources/variables/namespace.scss")
+        css(minimize:false, "/distribution-helper/namespace-override.scss")
         css(minimize:false, "/sources/variables/**.scss")
         css(minimize:false, "/distribution-helper/overrides.scss")
         css(minimize:false, "/sources/**.scss")

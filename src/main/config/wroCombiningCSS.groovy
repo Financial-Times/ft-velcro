77a85dff05145d1bc10db00848d954f8092d8c63
Jozsef Kerekes <jozsef.kerekes@ft.com>
2013-04-24 11:36:16 +0300

    IP957: US30379: Separate Velcro from ft-cms
    Moving font files from ft-cms to Velcro

--- src/main/config/wroCombiningCSS.groovy
+++ src/main/config/wroCombiningCSS.groovy
@@ -2,6 +2,7 @@ groups {
 
     "ft-velcro" {
         css(minimize:false, "/distribution-helper/base-variables.scss")
+        css(minimize:false, "/sources/variables/namespace.scss")
         css(minimize:false, "/sources/variables/**.scss")
         css(minimize:false, "/distribution-helper/overrides.scss")
         css(minimize:false, "/sources/**.scss")
